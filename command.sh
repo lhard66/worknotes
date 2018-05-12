18336ccb2deacecc38bc8025ee07ce193923c724
Allen Liu <lhard@qq.com>
2018-05-12 23:59:31 +0800

    log

---
+++ command.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# 保存当前目录
+currentDir=$PWD
+echo "Start to publish...\n"
+# 切换到项目目录
+#cd /usr/local/nginx/html/blog
+# 执行git命令
+git add .
+git commit -m 'log'
+git push origin master
+# 删除缓存 
+echo "Success\n";
