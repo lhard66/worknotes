7d9ebb1414e7dd496f0a4808836d373176610791
Allen Liu <lhard@qq.com>
2018-05-13 00:20:46 +0800

    log

--- command.bat
+++ command.bat
@@ -1,14 +1,8 @@
-#! /bin/bash
-# 保存当前目录
-F:
+cd F:
 cd hualala/doc/jgrmDocuments
 echo "Start to publish...\n"
-# 切换到项目目录
-#cd /usr/local/nginx/html/blog
-# 执行git命令
 git add .
 git commit -m 'log'
 git push origin master
-# 删除缓存 
 echo "Success\n";
 pause
