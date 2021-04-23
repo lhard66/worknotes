690ef2aa667c4bc64d3040f8f8c29efa197d091a
LiuTianqing <lhard@qq.com>
2021-04-23 14:06:24 +0800

    docs: plan 2021-04-23 14:06.24

--- tools/doclist.sh
+++ tools/doclist.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-# 保存当前目录
-cd F:/github/worknotes
+
 echo -e "\nHappy new day!\n"
+# 保存当前目录
+# cd F:/github/worknotes
+cd /Users/allen/github/worknotes
 # 切换到项目目录
 #cd /usr/local/nginx/html/blog
 # 执行git命令
